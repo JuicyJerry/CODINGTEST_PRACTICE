deac2d4a471ef4eb2b9130777fd2b34e49d5ac1d
JuicyJerry <54949453+JuicyJerry@users.noreply.github.com>
2021-04-15 00:37:15 +0900

    day 30

--- DAY30[프로그래머스][level2]스킬트리.js
+++ DAY30[프로그래머스][level2]스킬트리.js
@@ -1,23 +1,23 @@
 function solution(skill, skill_trees) {
-  let answer = 0;
-  let idx = 0;
-  let obj = {};
-  let prev = -1;
-
-  for (let j = 0; j < skill_trees.length; j++) {
-    for (let i = 0; i < skill.length; i++) {
-      if (skill_trees[j].indexOf(skill[i]) > -1 && j === 0) {
-        prev = skill_trees[j].indexOf(skill[i]);
-        // console.log(skill_trees[j].indexOf(skill[i]));
-        console.log(prev);
-      } else {
-        if (skill_trees[j].indexOf(skill[i]) > prev) skill_trees.splice(j, 1);
-        continue;
-      }
+    let answer = 0;
+    let idx = 0;
+    let obj = {};
+    
+    for (let j = 0; j < skill_trees.length; j++) {
+        for (let i = 0; i < skill.length; i++) {
+            if (skill_trees[j].indexOf(skill[i]) > -1 && j === 0) {
+                obj[i] = skill_trees[j].indexOf(skill[i]);
+                // console.log(skill_trees[j].indexOf(skill[i]));
+            } else if (skill_trees[j].indexOf(skill[i]) < obj[i]) {
+                skill_trees.splice(j, 1);
+                console.log("else")
+                console.log(skill_trees)
+                continue;
+            }
+        }
     }
-  }
-
-  return skill_trees.length;
-
-  // return answer;
+    
+    return skill_trees.length;
+    
+    // return answer;
 }
