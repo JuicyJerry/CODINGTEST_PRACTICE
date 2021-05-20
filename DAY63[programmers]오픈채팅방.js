34a94d7faaa2518b73909e0731b3d60e4659bae3
Jeong Hwan Lee <54949453+JuicyJerry@users.noreply.github.com>
2021-05-20 18:20:04 +0900

    day 63

--- DAY63[programmers]오픈채팅방.js
+++ DAY63[programmers]오픈채팅방.js
@@ -1,31 +1,23 @@
 function solution(record) {
   let obj = {};
-  let newRecord = new Array(record.length).fill(null);
-
-  record.map((el, idx) => {
-    newRecord[idx] = el.split(" ");
-  });
+  let newRecord = record.map((el) => el.split(" "));
 
   for (let i = 0; i < newRecord.length; i++) {
-    obj[newRecord[i][1]] = newRecord[i][2];
-  }
-
-  let ret = [];
-  let enter = `님이 들어왔습니다.`;
-  let exit = `님이 나갔습니다.`;
-  for (let i = 0; i < newRecord.length; i++) {
-    // console.log(newRecord[i][0]);
-
-    if (newRecord[i][0] === "Enter") {
-      ret.push(`${obj[newRecord[i][1]] + enter}`);
-    } else if (newRecord[i][0] === "Leave") {
-      ret.push(`${obj[newRecord[i][1]] + exit}`);
-    } else if (newRecord[i][0] === "Change") {
+    if (newRecord[i].length === 3) {
+      // 이걸 추가하고 통과 : leave에는 [2]가 없다
       obj[newRecord[i][1]] = newRecord[i][2];
     }
   }
 
+  let ret = [];
+  // let enter = ``;
+  // let exit = ``;
+  for (let i = 0; i < newRecord.length; i++) {
+    if (newRecord[i][0] === "Enter") {
+      ret.push(obj[newRecord[i][1]] + "님이 들어왔습니다.");
+    } else if (newRecord[i][0] === "Leave") {
+      ret.push(obj[newRecord[i][1]] + "님이 나갔습니다.");
+    }
+  }
   return ret;
-  // console.log(`obj: `, obj);
-  // console.log(`ret: `, ret);
 }
