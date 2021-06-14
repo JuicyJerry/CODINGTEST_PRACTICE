2ab07b59cbfb4c412597dd840a97b4b9716f0f55
Jeong Hwan Lee <54949453+JuicyJerry@users.noreply.github.com>
2021-06-15 00:16:50 +0900

    day 81

--- DAY81[programmers]스킬트리.js
+++ DAY81[programmers]스킬트리.js
@@ -1,5 +1,5 @@
 function solution(skill, skill_trees) {
-  /*
+    /*
         loop start
         2중 루프
         바깥 포문은 skill 기준
@@ -16,38 +16,42 @@ function solution(skill, skill_trees) {
         return cnt;
         종료
     */
-  let arr = new Array(skill_trees.length).fill("");
-
-  for (let y = 0; y < skill.length; y++) {
-    for (let x = 0; x < skill_trees.length; x++) {
-      let elementIdx = skill_trees[x].indexOf(skill[y]);
-
-      if (elementIdx > -1) {
-        arr[x] += elementIdx;
-      } else {
-        arr[x] += "x";
-      }
-    }
-  }
-  console.log(arr);
-
-  let cnt = 0;
-  for (let i = 0; i < arr.length; i++) {
-    let flag = 0;
-
-    if (arr[i].length !== skill.length) {
-    } else {
-      for (let j = 0; j < arr[i].length; j++) {
-        if (arr[i][j] > arr[i][j + 1]) {
-          flag = 1;
-          break;
+    let arr = new Array(skill_trees.length).fill('');
+    
+    for (let y = 0; y < skill.length; y++) {
+        
+        for (let x = 0; x < skill_trees.length; x++) {
+            let elementIdx = skill_trees[x].indexOf(skill[y]);
+            
+            if ( elementIdx > -1) {
+                arr[x] += elementIdx;  
+            } else {
+                arr[x] += 'x';
+            }
         }
-      }
-      if (flag === 0) {
-        cnt += 1;
-      }
     }
-  }
-
-  return cnt;
+    console.log(arr);
+    
+    let cnt = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let flag = 0;
+        
+        if (arr[i].length !== skill.length) {
+            for (let j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] === 'x' && arr[i][j]) break;
+            }
+        } else {
+            for (let j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > arr[i][j + 1]) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag === 0) {
+                cnt += 1;
+            }
+        }
+    }
+    
+    return cnt;
 }
