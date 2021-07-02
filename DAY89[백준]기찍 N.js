9e83cb02ada87c4884488c616f69eefc678c1406
Jeong Hwan Lee <54949453+JuicyJerry@users.noreply.github.com>
2021-07-02 23:59:18 +0900

    day 89

---
+++ DAY89[백준]기찍 N.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stout,
+});
+
+rl.on("line", function (line) {
+  line = Number(line);
+
+  for (let i = line; i >= 1; i--) {
+    console.log(i);
+  }
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
