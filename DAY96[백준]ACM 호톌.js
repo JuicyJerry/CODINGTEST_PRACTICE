e66339ac21ef10f95614ce6f45b3b9345f2a1172
Jeong Hwan Lee <54949453+JuicyJerry@users.noreply.github.com>
2021-07-09 14:57:56 +0900

    DAY 96

---
+++ DAY96[백준]ACM 호톌.js
@@ -0,0 +1,43 @@
+/*
+    방 번호: YXX / YYXX (Y || YY: 층 수, XX: 엘베부터 카운팅)
+    걷는 거리가 같을 때, 아래층 방 선호
+    모든 방이 비어있다고 가정
+    
+    N번째 도착 손님에게 배정될 방 번호 계산
+    
+    H: 6, W: 12
+*/
+
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let [len, guest1Info, guest2Info] = input;
+let [h1, w1, n1] = guest1Info.split(" ").map((el) => Number(el));
+let [h2, w2, n2] = guest2Info.split(" ").map((el) => Number(el));
+let room1, room2;
+let cnt = 0;
+
+for (let x = 1; x <= w1; x++) {
+  for (let y = 1; y <= h1; y++) {
+    if (cnt === n1) {
+      if (y < 10) {
+        y = `0${y}`;
+      } else if (x < 10) {
+        x = `0${x}`;
+      }
+      room1 = `${y}${x}`;
+    }
+  }
+}
+
+cnt = 0;
+for (let x = 1; x <= w1; x++) {
+  for (let y = 1; y <= h1; y++) {
+    if (cnt === n2) {
+      if (y < 10) y = `0${y}`;
+      if (x < 10) x = `0${x}`;
+      room2 = `${y}${x}`;
+    }
+  }
+}
+
+console.log(room1);
+console.log(room2);
